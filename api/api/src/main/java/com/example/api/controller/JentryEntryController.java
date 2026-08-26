871ea4406a8e935b9bbeb1064f042874595b0135
Atharva <v.atharvan@gmail.com>
2026-08-27 00:54:11 +0530

    Created api and use get put post delete methods using postman

---
+++ api/api/src/main/java/com/example/api/controller/JentryEntryController.java
@@ -0,0 +1,41 @@
+package com.example.api.controller;
+import java.util.*;
+
+import com.example.api.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/journal")
+public class JentryEntryController {
+
+    private Map<Long,JournalEntry> journalEntrys = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getall(){
+        return new ArrayList<>(journalEntrys.values());
+    }
+
+    @PostMapping
+    public String createentry(@RequestBody JournalEntry myentry) {
+        journalEntrys.put(myentry.getId(), myentry);
+        return "entry success";
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getById (@PathVariable long myId) {
+        return journalEntrys.get(myId);
+    }
+
+
+    @DeleteMapping ("id/{myId}")
+    public JournalEntry deletById (@PathVariable long myId) {
+        return journalEntrys.get(myId);
+    }
+
+
+
+}
